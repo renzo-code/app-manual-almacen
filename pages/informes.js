ca6e7f475bf01062e36e385374b4a31bc53e8daf
unknown <alejomayuri@gmail.com>
2021-08-09 12:16:39 -0500

    informes y logica

--- pages/informes.js
+++ pages/informes.js
@@ -1,11 +1,61 @@
-import Layout from "../layouts/default"
+import Layout from '../layouts/default'
+import ContentInformes from '../components/ContentInformes/default'
+import { prop } from 'ramda'
+import styled from 'styled-components'
 
-const Informes = () => {
+const URL = process.env.REACT_APP_ENDPOINT
+const TOKEN_ID = process.env.REACT_APP_TOKEN
+
+const Home = ({ article }) => {
+  const DataSelecionEditor = prop("data", prop("packages", prop("data", article)))
+  console.log(DataSelecionEditor)
   return (
     <Layout>
-      Hola soy informes
+      <Content>
+        <ContentInformes
+          DataSelecionEditor={DataSelecionEditor} 
+        />
+      </Content>
     </Layout>
   )
 }
 
-export default Informes
+Home.getInitialProps = async (ctx) => {
+  let responseSelecionEditor = {}
+  let responseEspeciales = {}
+  const { REACT_APP_ENDPOINT, REACT_APP_TOKEN } = process.env
+
+  const object_fetch = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/menu',
+      'token_id': REACT_APP_TOKEN
+    }
+  }
+
+  try {
+
+    responseSelecionEditor = await fetch(`${REACT_APP_ENDPOINT}/api/content/package/list?site_id=larepublica&status=1&order_field=updated_at&subdomain=data`, object_fetch)
+    responseSelecionEditor = await responseSelecionEditor.json()
+
+    responseEspeciales = await fetch(`${REACT_APP_ENDPOINT}/api/spotlight?site_id=larepublica&_id=60b5442e258c30219c461fe3&easy=1&status=1`, object_fetch)
+    responseEspeciales = await responseEspeciales.json()
+
+  } catch (e) {
+    console.error(e)
+  }
+
+  return {
+    article: responseSelecionEditor,
+
+    articleEspeciales : responseEspeciales,
+  }
+}
+
+export default Home
+
+const Content = styled.div`
+  width: 100%;
+  height: auto;
+  background-color: #252525;
+`
